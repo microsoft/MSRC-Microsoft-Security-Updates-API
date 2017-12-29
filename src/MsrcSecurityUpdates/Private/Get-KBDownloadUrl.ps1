0b133f46725b9a54e5b9fe247ef07129b8b7541b
Emin <emin.atac@gmail.com>
2017-12-29 21:03:01 +0100

    minor fixes in Get-KBDownloadUrl

--- src/MsrcSecurityUpdates/Private/Get-KBDownloadUrl.ps1
+++ src/MsrcSecurityUpdates/Private/Get-KBDownloadUrl.ps1
@@ -10,32 +10,34 @@ Function Get-KBDownloadUrl {
         The KB Article object that contains the id, url, and subtype.
 
     .EXAMPLE
-        [PSCustomObject]{ID="kb123456"; URL="microsoft.com"; SubType="Required"} | Get-KBDownloadUrl
+        [PSCustomObject]@{ID="kb123456"; URL="microsoft.com"; SubType="Required"} | Get-KBDownloadUrl
 #>
-    [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory,ValueFromPipeline)]
-        [PSCustomObject]$KBArticleObject
-    )
-    Begin {
-        $HTML_TO_RETURN = @()
-    }
-    Process {
-        if (-not($KBArticleObject)){
-            'None'
-        } else {
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory,ValueFromPipeline)]
+    [PSCustomObject]$KBArticleObject
+)
+Begin {
+    $HTML_TO_RETURN = @()
+}
+Process {
+    if (-not($KBArticleObject)){
+        'None'
+    } else {
         
-            foreach($kb in $KBArticleObject){
-                #In older months, there won't be a subtype. Handle this so there are not empty ()'s
-                if($KBArticleObject.SubType -ne $null){
-                    $HTML_TO_RETURN += $('<a href="{0}" >{1} ({2})' -f $kb.URL, $kb.ID, $kb.SubType)
-                } else {
-                    $HTML_TO_RETURN += $('<a href="{0}" >{1}' -f $kb.URL, $kb.ID)
-                }
+        $KBArticleObject | 
+        ForEach-Object {
+            $kb = $_
+            #In older months, there won't be a subtype. Handle this so there are not empty ()'s
+            if($kb.SubType){
+                $HTML_TO_RETURN += $('<a href="{0}" >{1} ({2})' -f $kb.URL, $kb.ID, $kb.SubType)
+            } else {
+                $HTML_TO_RETURN += $('<a href="{0}" >{1}' -f $kb.URL, $kb.ID)
             }
         }
     }
-    End {
-        return $HTML_TO_RETURN -join '<br />'
-    }
+}
+End {
+    $HTML_TO_RETURN -join '<br />'
+}
 }
