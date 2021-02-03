c3c2ed5331a9f8dc35e5066818ca05d18866e863
Stefani Moore <stmoor@microsoft.com>
2021-02-03 13:36:26 -0800

    fixing error with getting access token

--- src/MsrcSecurityUpdates/Public/Set-MsrcAdalAccessToken.ps1
+++ src/MsrcSecurityUpdates/Public/Set-MsrcAdalAccessToken.ps1
@@ -12,11 +12,13 @@ Process {
 
         $promptBehavior = [Microsoft.IdentityModel.Clients.ActiveDirectory.PromptBehavior]::Auto
 
+        $platformParams = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters($promptBehavior)
+
         $ResourceId = 'https://msrc-api-prod.azurewebsites.net'
 
         $ClientId = 'c7fe3b9e-4d97-462d-ae1b-c16e679be355'
 
-        $authResult = $authContext.AcquireToken($ResourceId, $ClientId, $rUri,$promptBehavior)
+        $authResult = $authContext.AcquireTokenAsync($ResourceId, $ClientId, $rUri,$platformParams).GetAwaiter().GetResult()
 
 	    $global:MSRCAdalAccessToken = $authResult
         Write-Verbose -Message "Successfully set your Access Token required by cmdlets of this module.    Calls to the MSRC APIs will now use your access token."
