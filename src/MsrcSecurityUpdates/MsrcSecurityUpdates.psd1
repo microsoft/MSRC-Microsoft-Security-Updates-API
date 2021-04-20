85b18e5d23ceb7cc9eccbefc153e3960afeb8f50
Stefani Moore <stmoor@microsoft.com>
2021-04-20 14:17:23 -0700

    update api endpoint

--- src/MsrcSecurityUpdates/MsrcSecurityUpdates.psd1
+++ src/MsrcSecurityUpdates/MsrcSecurityUpdates.psd1
@@ -12,7 +12,7 @@
 RootModule = 'MsrcSecurityUpdates.psm1'
 
 # Version number of this module.
-ModuleVersion = '1.9.4'
+ModuleVersion = '1.9.5'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
--- src/MsrcSecurityUpdates/Private/Set-GlobalVariables.ps1
+++ src/MsrcSecurityUpdates/Private/Set-GlobalVariables.ps1
@@ -1,5 +1,5 @@
 ﻿# we also set other shared variables
-$global:msrcApiUrl     = 'https://api.msrc.microsoft.com'
+$global:msrcApiUrl     = 'https://api.msrc.microsoft.com/cvrf/v2.0'
 Write-Verbose -Message "Successfully defined a msrcApiUrl global variable that points to $($global:msrcApiUrl)"
 
 $global:msrcApiVersion = 'api-version=2016-08-01'
