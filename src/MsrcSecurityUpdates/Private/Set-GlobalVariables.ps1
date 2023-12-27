6855fb82ec87d1c00d44d1b3e494510b9279c842
Emin <emin.atac@gmail.com>
2023-12-27 22:28:11 +0100

    Adding changes from tsharp's  commit aa0e84b #125

--- src/MsrcSecurityUpdates/Private/Set-GlobalVariables.ps1
+++ src/MsrcSecurityUpdates/Private/Set-GlobalVariables.ps1
@@ -1,6 +1,6 @@
 ﻿# we also set other shared variables
-$global:msrcApiUrl     = 'https://api.msrc.microsoft.com/cvrf/v2.0'
+$global:msrcApiUrl     = 'https://api.msrc.microsoft.com/cvrf/v3.0'
 Write-Verbose -Message "Successfully defined a msrcApiUrl global variable that points to $($global:msrcApiUrl)"
 
-$global:msrcApiVersion = 'api-version=2016-08-01'
+$global:msrcApiVersion = 'api-version=2023-11-01'
 Write-Verbose -Message "Successfully defined a msrcApiVersion global variable that points to $($global:msrcApiVersion)"
